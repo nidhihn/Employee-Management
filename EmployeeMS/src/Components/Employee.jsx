d6285dab8f9edbf5e75c64a12b6cae054aae474f
Nidhi H N <nidhinshetty28@gmail.com>
2024-03-13 01:47:25 +0530

    Added dashboard design

--- EmployeeMS/src/Components/Employee.jsx
+++ EmployeeMS/src/Components/Employee.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import React, { useEffect ,useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const Employee = () => {
   const [employee, setEmployee] = useState([])
+  const navigate = useNavigate()
+
   useEffect(() => {
     axios.get("http://localhost:3000/auth/employee")
       .then((result) => {
@@ -16,6 +18,16 @@ const Employee = () => {
       })
       .catch((err) => console.log(err));
   }, []);
+  const handleDelete = (id) => {
+    axios.delete('http://localhost:3000/auth/delete_employee/'+id)
+    .then(result => {
+      if(result.data.Status){
+        window.location.reload()
+      } else {
+        alert(result.data.Error)
+      }
+    })
+  }
   return (
     <div className="px-5 mt-3">
       <div className="d-flex justify-content-center">
@@ -38,16 +50,17 @@ const Employee = () => {
                     </thead>
                     <tbody>
                         {
-                            employee.map(e => (
-                                <tr>
+                            employee.map((e,i) => (
+                              
+                                <tr key={i}>
                                     <td>{e.name}</td>
                                     <td><img src={`http://localhost:3000/Images/`+e.image} alt="" className="employee_image"/></td>
                                     <td>{e.email}</td>
                                     <td>{e.address}</td>
                                     <td>{e.salary}</td>
                                     <td>
-                                      <button className="btn btn-info btn-sm me-2">Edit</button>
-                                      <button className="btn btn-warning btn-sm">Delete</button>
+                                      <Link to={`/dashboard/edit_employee/`+ e.id} className="btn btn-info btn-sm me-2">Edit</Link>
+                                      <button className="btn btn-warning btn-sm" onClick={() => handleDelete(e.id)}>Delete</button>
                                     </td>
                                 </tr>
                             ))
